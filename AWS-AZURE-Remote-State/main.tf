c709b6e8b1f9268f749afe13a7ee964083b3ce57
dennis.gusmao <dennis.gusmao@aquare.la>
2024-05-02 10:06:15 -0300

    Aula 31

--- AWS-AZURE-Remote-State/main.tf
+++ AWS-AZURE-Remote-State/main.tf
@@ -1,15 +1,32 @@
 terraform {
-  required_version = ">=1.7.4"
+  required_version = ">= 1.3.0"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.48.0"
+required_providers {
+  aws = {
+    source = "hashicorp/aws"
+    version = "4.60.0"
+  }
+
+  azurerm = {
+    source = "hashicorp/azurerm"
+    version = ">=3.48.0"
+  }
+}
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform-curso"
+
+  default_tags {
+    tags = {
+      owner = "dennisgusmao"
+      managed-by = "terraform"
     }
   }
 }
+
 provider "azurerm" {
   features {}
   skip_provider_registration = "true"
 }
-
