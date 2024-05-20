3d49cbccdf1377c82af247e5ab52d5e9279c7feb
dennis.gusmao <dennis.gusmao@aquare.la>
2024-05-20 09:12:12 -0300

    Aula 52 Depends_on

---
+++ depends-on-modulos-remotos/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.48.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "dennisterraformstate"
+    container_name       = "remote-state-azure"
+    key                  = "depends-on/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = "true"
+}
+
+
+module "network" {
+  depends_on = [
+    azurerm_resource_group.resource_group
+   ]
+  source = "Azure/network/azurerm"
+  version = "5.2.0"
+
+  resource_group_name = azurerm_resource_group.resource_group.name
+  use_for_each = true
+  vnet_name = "vnet-${var.environment}" 
+  subnet_names = ["subnet-${var.environment}"]
+  tags =  local.common_tags
+}
