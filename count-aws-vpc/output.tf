6138e0f33d4b6fa1f27c7e1eb4eb7207b235c2d0
dennis.gusmao <dennis.gusmao@aquare.la>
2024-05-20 09:22:33 -0300

    Aula 53 Count

---
+++ count-aws-vpc/output.tf
@@ -0,0 +1,14 @@
+output "subnet_id_0" {
+  description = "Id da subnet criada na AWS"
+  value = aws_subnet.subnet[0].id
+}
+
+output "subnet_id_1" {
+  description = "Id da subnet criada na AWS"
+  value = aws_subnet.subnet[1].id
+}
+
+output "subnet_id_2" {
+  description = "Id da subnet criada na AWS"
+  value = aws_subnet.subnet[1].id
+}
