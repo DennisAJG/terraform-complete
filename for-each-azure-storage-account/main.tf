83944c4453e7fd8aa17f67203a8fc9e6b116665c
dennis.gusmao <dennis.gusmao@aquare.la>
2024-05-27 09:55:43 -0300

    54 uso do For_each

---
+++ for-each-azure-storage-account/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.48.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name = "rg-terraform-state"
+    storage_account_name = "danielgilterraformstate"
+    container_name = "remote-state"
+    key = "for-each/terraform.tfstate"
+  }
+}
+
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  skip_provider_registration = "true"
+}
+
+#OBS: A Azure não permite criar tags padrões dentro do bloco Main
