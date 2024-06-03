c30e4e36b62d870b72c3b042e0a53151470f5d17
dennis.gusmao <dennis.gusmao@aquare.la>
2024-06-03 15:47:04 -0300

    Aula 61 terraform console

---
+++ terraform-console/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  common_tags = {
+    owner      = "dennisgusmao"
+    managed-by = "terraform"
+    location = var.location
+  }
+}
---
+++ terraform-console/outputs.tf
@@ -0,0 +1,4 @@
+output "g_id" {
+  description = "ID da resource group criada na Azure"
+  value       = azurerm_resource_group.resource_group.id
+}
