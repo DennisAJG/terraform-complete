1eec911689ef8b7ba1ec54a9e21adfae329ae897
dennis.gusmao <dennis.gusmao@aquare.la>
2024-07-10 09:34:10 -0300

    Aula 70 Terraform em Pipeline Gitlab CI

---
+++ pipeline-gitlab-ci/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">=5.16.2"
+    }
+
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">=3.72.0"
+    }
+  }
+
+# Só pode escolher um lugar para onde vai o backend
+  backend "azurerm" {
+    resource_group_name = "rg-terraform-state"
+    storage_account_name = "dennisgusmaoterraformstate"
+    container_name = "remote-state"
+    key = "pipeline-gitlab/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+    default_tags {
+      tags = {
+        owner = "dennisgusmao"
+        managed-by = "terraform"
+      }
+    }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = "true"
+}
+
+
+
+data "terraform_remote_state" "vpc" {
+    backend = "s3"
+    config = {
+      bucket = "dennisgusmao-remote-state"
+      key = "aws-vpc/terraform.tfstate"
+      region = "us-east-1"
+    }
+}
+
+data "terraform_remote_state" "vnet" {
+  backend = "azurerm"
+  config = {
+    resource_group_name = "rg-terraform-state"
+    storage_account_name = "dennisgusmaoterraformstate"
+    container_name = "remote-state"
+    key = "azure-vnet/terraform.tfstate"
+  }
+}
